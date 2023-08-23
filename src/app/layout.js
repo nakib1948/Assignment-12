e0ed38587ab08e939bc168a4ef8e3090f9abe046
nakib1948 <hayathossain.jtp@gmail.com>
2023-08-23 08:49:41 +0600

    font added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import Navbar from '@/Components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Hind_Siliguri, Inter } from 'next/font/google'
 import Footer from '@/Components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,10 +9,14 @@ export const metadata = {
   title: 'Blog website',
   description: 'Find latest blog',
 }
-
+const Siliguri = Hind_Siliguri({
+  subsets: ['bengali'],
+  weight: ['300','400','500','600','700'],
+  variable:'--font-siliguri'
+})
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={Siliguri.variable}>
       <body className={inter.className}>
         <Navbar/>
         {children}
